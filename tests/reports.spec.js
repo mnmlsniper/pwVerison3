826c55edec35a1a616307341a5492dd185b8870f
Your Name <you@example.com>
2025-05-06 19:31:01 +0300

    add allure

---
+++ tests/reports.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import { UserBuilder } from '../src/helpers/builders/user.builder';
+import { MainPage } from '../src/pages/main.page';
+import { RegisterPage } from '../src/pages/register.page';
+import { YourFeedPage } from '../src/pages/yourfeed.page';
+
+test('Авторизованный пользователь может просмотреть список статей', async ({
+	page,
+}) => {
+	// Готовим страницы
+	const mainPage = new MainPage(page);
+	const registerPage = new RegisterPage(page);
+	const yourFeedPage = new YourFeedPage(page);
+
+	// Act
+	const randomUser = new UserBuilder()
+		.addEmail()
+		.addPassword(14)
+		.addUsername()
+		.generate();
+
+	await mainPage.open();
+	await mainPage.gotoLogin();
+	await registerPage.signup(randomUser);
+	await expect(yourFeedPage.profileNameField).toContainText(
+		randomUser.username,
+	);
+});
